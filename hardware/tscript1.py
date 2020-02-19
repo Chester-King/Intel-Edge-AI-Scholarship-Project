f11cdaf049a4ce68b87987615bd01d7c9ef3f11a
Madhur Dixit <dmadhur6@gmail.com>
2020-02-20 03:00:42 +0530

    Model Changed, Human + Car detection on video

--- hardware/tscript1.py
+++ hardware/tscript1.py
@@ -93,6 +93,11 @@ def run_app():
         print(detections)
         # refined_detections = []
         for detection in detections:
+
+            print("*********************************************")
+            print(detection)
+            print("*********************************************")
+
             if(detection[2] > arguments.detection_threshold):
                 # refined_detections.append(detection)
                 print(detection)
@@ -103,8 +108,12 @@ def run_app():
                 ymin = int(detection[4] * fh)
                 xmax = int(detection[5] * fw)
                 ymax = int(detection[6] * fh)
-                cv.rectangle(image, (xmin, ymin),
-                             (xmax, ymax), (0, 125, 255), 3)
+                if(detection[1] == 1):
+                    cv.rectangle(frame, (xmin, ymin),
+                                 (xmax, ymax), (0, 255, 0), 3)
+                if(detection[1] == 2):
+                    cv.rectangle(frame, (xmin, ymin),
+                                 (xmax, ymax), (0, 0, 255), 3)
             else:
                 break
                 # if detection[2] > arguments.detection_threshold:
@@ -123,6 +132,161 @@ def run_app():
         cv.imshow('Window', image)
         cv.waitKey(0)
 
+    else:
+        print("Running Inference for {} - {}".format(arguments.input_type, arguments.input))
+
+        process_id = os.getpid()
+        process = psutil.Process(process_id)
+
+        total_inference_time = 0.0
+        # Implementation for CAM or Video File
+        # Read Image
+        capture = cv.VideoCapture(arguments.input)
+        has_frame, frame = capture.read()
+        frame_count += 1
+
+        if not has_frame:
+            print("Can't Open Input Video Source {}".format(arguments.input))
+            exit(-1)
+
+        # Get Shape Values
+        N, C, H, W = OpenVinoNetwork.inputs[InputLayer].shape
+        fh = frame.shape[0]
+        fw = frame.shape[1]
+        print('Original Frame Shape: ', fw, fh)
+
+        request_order = list()
+        process_order = list()
+        frame_order = list()
+        if arguments.async:
+            print("Async Mode Set")
+            for i in range(number_of_async_req):
+                request_order.append(i)
+                print('Request Id {} Created'.format(i))
+
+            print('Request Ids {}'.format(request_order))
+
+        while has_frame:
+            if arguments.async:
+                if len(request_order) > 0:
+                    resized = cv.resize(frame, (W, H))
+                    # Change data layout from HWC to CHW
+                    resized = resized.transpose((2, 0, 1))
+                    input_data = resized.reshape((N, C, H, W))
+                    req_id = request_order[0]
+                    request_order.pop(0)
+                    OpenVinoExecutable.start_async(
+                        req_id, inputs={InputLayer: input_data})
+                    process_order.append(req_id)
+                    frame_order.append(frame)
+
+                if len(process_order) > 0:
+                    first = process_order[0]
+                    if OpenVinoExecutable.requests[first].wait(0) == 0:
+                        results = OpenVinoExecutable.requests[first].outputs[OutputLayer]
+                        process_order.pop(0)
+                        request_order.append(first)
+                        show_frame = frame_order[0]
+                        frame_order.pop(0)
+
+                        detections = results[0][0]
+                        for detection in detections:
+
+                            if detection[2] > arguments.detection_threshold:
+
+                                xmin = int(detection[3] * fw)
+                                ymin = int(detection[4] * fh)
+                                xmax = int(detection[5] * fw)
+                                ymax = int(detection[6] * fh)
+                                if(detection[1] == 1):
+                                    cv.rectangle(frame, (xmin, ymin),
+                                                 (xmax, ymax), (0, 255, 0), 3)
+                                if(detection[1] == 2):
+                                    cv.rectangle(frame, (xmin, ymin),
+                                                 (xmax, ymax), (0, 0, 255), 3)
+
+                        fps = frame_count / (time.time() - start_time)
+                        # Write Information on Image
+                        text = 'FPS: {}, INF: {} ms'.format(round(fps, 3), "-")
+                        cv.putText(show_frame, text, (0, 20),
+                                   cv.FONT_HERSHEY_COMPLEX, 0.8, (0, 125, 255), 1)
+
+                        text = "SYS CPU% {} SYS MEM% {} \n " \
+                               "PROC CPU Affinity {} \n " \
+                               "NUM Threads {} \n " \
+                               "PROC CPU% {} \n " \
+                               "PROC MEM% {}".format(psutil.cpu_percent(),
+                                                     psutil.virtual_memory()[
+                                   2],
+                                   process.cpu_affinity(),
+                                   process.num_threads(),
+                                   process.cpu_percent(),
+                                   round(process.memory_percent(), 4))
+
+                        cv.putText(show_frame, text, (0, 50),
+                                   cv.FONT_HERSHEY_COMPLEX, 1, (250, 0, 255), 1)
+                        cv.imshow('Window', show_frame)
+                        if cv.waitKey(1) & 0xFF == ord('q'):
+                            break
+
+                if len(process_order) > 0:
+                    has_frame, frame = capture.read()
+                    frame_count += 1
+            else:
+                frame_count += 1
+                resized = cv.resize(frame, (W, H))
+                # Change data layout from HWC to CHW
+                resized = resized.transpose((2, 0, 1))
+                input_data = resized.reshape((N, C, H, W))
+                # Start Inference
+                results = OpenVinoExecutable.infer(
+                    inputs={InputLayer: input_data})
+
+                fps = frame_count / (time.time() - start_time)
+                inf_time = (time.time() - start_time) / frame_count
+                # Write Information on Image
+                text = 'FPS: {}, INF: {} ms'.format(
+                    round(fps, 3), round(inf_time, 3))
+                cv.putText(frame, text, (0, 20),
+                           cv.FONT_HERSHEY_COMPLEX, 0.8, (0, 125, 255), 1)
+
+                # Print Bounding Boxes on Image
+                detections = results[OutputLayer][0][0]
+                for detection in detections:
+
+                    if detection[2] > arguments.detection_threshold:
+
+                        xmin = int(detection[3] * fw)
+                        ymin = int(detection[4] * fh)
+                        xmax = int(detection[5] * fw)
+                        ymax = int(detection[6] * fh)
+
+                        if(detection[1] == 1):
+                            cv.rectangle(frame, (xmin, ymin),
+                                         (xmax, ymax), (0, 255, 0), 3)
+                        if(detection[1] == 2):
+                            cv.rectangle(frame, (xmin, ymin),
+                                         (xmax, ymax), (0, 0, 255), 3)
+                        detection_percentage = round(detection[2], 4)
+
+                text = "SYS CPU% {} SYS MEM% {} \n " \
+                       "PROC CPU Affinity {} \n " \
+                       "NUM Threads {} \n " \
+                       "PROC CPU% {} \n " \
+                       "PROC MEM% {}".format(psutil.cpu_percent(),
+                                             psutil.virtual_memory()[2],
+                                             process.cpu_affinity(),
+                                             process.num_threads(),
+                                             process.cpu_percent(),
+                                             round(process.memory_percent(), 4))
+
+                cv.putText(frame, text, (0, 50),
+                           cv.FONT_HERSHEY_COMPLEX, 0.8, (250, 0, 250), 1)
+                cv.imshow('Window', frame)
+                if cv.waitKey(1) & 0xFF == ord('q'):
+                    break
+                has_frame, frame = capture.read()
+
     print("---------")
     print("App ENDS")
 
@@ -133,17 +297,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Open VINO vehical detection ADAS')
     parser.add_argument('--model-xml',
-                        default='D:/Work/IntelOpenVINO/vehicle-detection-adas-model/vehicle-detection-adas-0002.xml',
+                        default='D:/Work/IntelOpenVINO/pedestrian-and-vehicle-detector-adas-0001/pedestrian-and-vehicle-detector-adas-0001.xml',
                         help='XML File')
     parser.add_argument('--model-bin',
-                        default='D:/Work/IntelOpenVINO/vehicle-detection-adas-model/vehicle-detection-adas-0002.bin',
+                        default='D:/Work/IntelOpenVINO/pedestrian-and-vehicle-detector-adas-0001/pedestrian-and-vehicle-detector-adas-0001.bin',
                         help='BIN File')
 
     parser.add_argument('--target-device', default='CPU',
                         help='Target Plugin: CPU, GPU, FPGA, MYRIAD, MULTI:CPU,GPU, HETERO:FPGA,CPU')
-    parser.add_argument('--input-type', default='image',
+    parser.add_argument('--input-type', default='video',
                         help='Type of Input: image, video, cam')
-    parser.add_argument('--input', default='D:/Work/IntelOpenVINO/videos/ti3.jpg',
+    parser.add_argument('--input', default='D:/Work/IntelOpenVINO/videos/Walk.mp4',
                         help='Path to Input: WebCam: 0, Video File or Image file')
 
     parser.add_argument('--detection-threshold', default=0.6,
